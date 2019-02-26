3282b65dabd4c0a99bde4d3ef8938d0b38553c72
DanFCo <danfcorreia@gmail.com>
2019-02-25 19:02:03 -0500

    the bachelor

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,72 @@
+
+require"pry"
+
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+name_of_winner = ""
+
+  data.each do |attributes,v|
+    if attributes == season
+        v.each do |k|
+          if k["status"] == "Winner"
+          name_of_winner = k["name"].split(" ")[0]
+          end
+        end
+      end
+    end
+    name_of_winner
+  end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_name = ""
+
+  data.each do |attributes,v|
+    v.each do|k|
+      if k["occupation"] == occupation
+        contestant_name = k["name"]
+      end
+    end
+  end
+ contestant_name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i=0
+  data.each do|attributes,v|
+    v.each do |k|
+      if k["hometown"] == hometown
+        i += 1
+      end
+    end
+  end
+  i
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation_of_contestant = ""
+
+   data.each do|attributes,v|
+     v.find do |k|
+       if k["hometown"] == hometown
+         occupation_of_contestant = k["occupation"]
+       end
+     end
+   end
+   occupation_of_contestant
 end
 
 def get_average_age_for_season(data, season)
-  # code here
-end
+    ages = []
+    average = ""
+  data.each do |attributes,v|
+    if attributes == season
+      v.each do |k|
+        ages.push(k["age"].to_i)
+
+         average = ages.inject {|sum, el| sum + el }.to_f / ages.size
+       end
+     end
+   end
+ average.round
+ end
